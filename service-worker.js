48f57727871964aa621e8131cb63fd0687c57f63
ShapovalDenys <denys.igorevich@gmail.com>
2020-08-04 13:02:07 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.0e892fa607c9b383e517161c0cb49d09.js"
+  "./precache-manifest.5a2e59a0ead46291b869922cf87e3ca5.js"
 );
 
 self.addEventListener('message', (event) => {
